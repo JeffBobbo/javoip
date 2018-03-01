d004afaadd92d5854722140f4d04cbdadd2efbdf
JeffBobbo <james.delph@hotmail.co.uk>
2018-03-01 19:02:50 +0000

    new mitigation stuff

--- src/Mitigation.java
+++ src/Mitigation.java
@@ -1,3 +1,6 @@
+import java.util.zip.Adler32;
+import java.util.zip.CRC32;
+import java.util.zip.Checksum;
 
 public class Mitigation
 {
@@ -25,5 +28,39 @@ public class Mitigation
     }
   }
 
+  /* Interleaves an array of square length, essentially performing a matrix transpose. This method is it's own inverse.
+   * @param data The data to interleave, length must natural square
+   * @param rows number of rows in the data
+   * @param columns number of columns in the data
+   */
+  public static void interleave(byte[] data, final int rows, final int columns)
+  {
+    for (int r = 0; r < rows; ++r)
+    {
+      for (int c = r; c < columns; ++c)
+      {
+        final int x = r*rows+c;
+        final int y = c*rows+r;
+        byte t = data[x];
+        data[x] = data[y];
+        data[y] = t;
+      }
+    }
+  }
+
+  public static int checksum(byte[] data)
+  {
+    chk.reset();
+    chk.update(data, 0, data.length);
+    return (int)chk.getValue();
+  }
+  public static void useCRC32() { chk = new CRC32(); }
+  public static void useAdler32() { chk = new Adler32(); }
+
   public static boolean useInterleaving = false;
+  public static final int INTERLEAVE_ROWS = 32;
+  public static final int INTERLEAVE_COLUMNS = 32;
+
+  public static boolean useChecksums = false;
+  private static Checksum chk = new CRC32();
 }
